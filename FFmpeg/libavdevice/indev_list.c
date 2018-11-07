b5f87ea77156592263a9caf0652a7056b5a13baa
Single <libobjc@gmail.com>
2018-11-08 01:30:59 +0800

    Add template/list/other files

---
+++ FFmpeg/libavdevice/indev_list.c
@@ -0,0 +1,4 @@
+static const AVInputFormat * const indev_list[] = {
+    &ff_avfoundation_demuxer,
+    &ff_lavfi_demuxer,
+    NULL };
---
+++ FFmpeg/libavdevice/outdev_list.c
@@ -0,0 +1,3 @@
+static const AVOutputFormat * const outdev_list[] = {
+    &ff_sdl2_muxer,
+    NULL };
